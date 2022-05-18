441bc6ae787f549d178a1dcaca4f46c5ab5d468e
OsziMisi <78954799+OsziMisi@users.noreply.github.com>
2022-05-18 11:33:27 +0200

    icon hozzáadása
    iconkép hozzáadása és

--- rombusz.py
+++ rombusz.py
@@ -1,22 +1,27 @@
 from tkinter import *
 from tkinter import messagebox
 import math
+import tkinter as tk
 
 
-foablak = Tk()
+
+
+foablak = tk.Tk()
 foablak.title('Síkidomok kerülete és területe ')
 foablak.minsize(width = 500, height=150)
+foablak.tk.call('wm', 'iconphoto', foablak._w, tk.PhotoImage(file='C:\\Users\\oroszlanmihaly\\Documents\\GitHub\\tkinter-projekt\\rombusz.png'))
 
-a = PhotoImage(file = 'rombusz.jpg')
-foablak.iconphoto(False, a)
 
-def terület():
-        def szamit():
-            a=eval(mezo1.get())
-            b=eval(mezo2.get())
-            felszin=a*b if a and b !=0 else "Hibas kerlek ne irj be mast"
-            mezo3.delete(0,END)
-            mezo3.insert(0, str(felszin))
 
+def terület():     
+    def szamit():
+        a=eval(mezo1.get())
+        b=eval(mezo2.get())
+        felszin=a*a*a*a if a and b !=0 else "Hibas kerlek ne irj be mast"
+        mezo3.delete(0,END)
+        mezo3.insert(0, str(felszin))
+
+ablak3=Toplevel(foablak)
+ablak3.title('Ez a rombusz kerülete és területe')
 foablak.mainloop()
 

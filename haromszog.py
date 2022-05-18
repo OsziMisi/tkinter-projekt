6b06062b58e86c2f6f4531a9eb7fa67cd1b5d583
Szaszuge <78954822+Szaszuge@users.noreply.github.com>
2022-05-18 11:10:31 +0200

    haromszog rajzolas pyturtle segitsegevel

--- haromszog.py
+++ haromszog.py
@@ -1,1 +1,16 @@
 from tkinter import *
+import turtle
+
+foablak = Tk()
+board = turtle.Turtle()
+ 
+board.forward(100) # draw base
+ 
+board.left(120)
+board.forward(100)
+ 
+board.left(120)
+board.forward(100)
+ 
+turtle.done()
+foablak.mainloop()
